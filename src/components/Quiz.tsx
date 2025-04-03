9cc83d007abda5aa11b5f0e21d74c31a5a9f0424
daffodil009 <daffodil.luck9@gmail.com>
2025-04-03 12:35:53 +0100

    Update Quiz component and improve layout responsiveness

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, {useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { useWindowSize } from 'react-use';
 
 import { Button } from "@/components/ui/button"
